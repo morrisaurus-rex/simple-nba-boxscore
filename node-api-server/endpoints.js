228f9393b8819a0d8850efa6bdf0bf1deaed3f73
Morris Li <li081197@gmail.com>
2022-06-08 22:50:13 -0700

    exported symbols

--- node-api-server/endpoints.js
+++ node-api-server/endpoints.js
@@ -1,38 +1,35 @@
-const Root= 'https://data.nba.net/10s/prod/v1';
-const Today = Root + '/today.json';
+exports.rootEndpoint = 'https://data.nba.net/10s/prod/v1';
+exports.todayEndpoint = exports.rootEndpoint + '/today.json';
 
 /**
  * Constructs an endpoint for the scoreboard of a given date. If no date is provided then it will use the current date instead.
  * @param {*} date *Optional*. Date in the format of *YYYYMMDD*. This will not be validated.
  * @returns URL endpoint for the scoreboard
  */
-function Scoreboard(date) {
-    if (date) {
-        return `${Root}/${date}/scoreboard.json`;
-    }
-    else {
-        let date = TodaysDate();
-        return `${Root}/${today}/scoreboard.json`;
-    }
-
+function scoreboard(date) {
+    if (!date) var date = todaysDate();
+    return `${Root}/${today}/scoreboard.json`;
 }
+exports.scoreboard = scoreboard;
+
 /**
  * Returns an endpoint for a game's play-by-play. Returns a blank string if `gameId` is not given.
  * @param {*} gameId ID of the desired game.
  * @param {*} date *Optional*. Date in the format of *YYYYMMDD*. This will not be validated.
  * @param {*} period *Optional*. Value from [1-4]. Defaults to 1.
  */
-function PlayByPlay(gameId, date, period) {
+function playByPlay(gameId, date, period) {
     if (!gameId) return "";
-    if (!date)  var date = TodaysDate();
+    if (!date)  var date = exports.todaysDate();
     if (!period) var period = 1;  
     return `${Root}/${date}/${gameId}_pbp_${period}.json`
 }
+exports.playByPlay = playByPlay;
 
 /**
  * Returns a string representation of today's date in *YYYYMMDD* format.
  */
-function TodaysDate() {
+function todaysDate() {
     let today = new Date();
     let month = today.getMonth() + 1;
     let year = today.getFullYear() + "";
@@ -45,3 +42,4 @@ function TodaysDate() {
     else today += "";
     return year + month + today;
 }
+exports.todaysDate = todaysDate;
